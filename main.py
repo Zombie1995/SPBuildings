130cb0e3a74df74bb5ad80ca6794704560f6ea16
Zombie1995 <62598313+Zombie1995@users.noreply.github.com>
2022-04-15 13:11:52 +0300

    init

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print("Hello World")
+import telebot
+bot = telebot.TeleBot('5302345860:AAGahsIU7Q6lAYz4tD5ZVVFMpqugRKTHXIE')
 
-print("hahah")
+name = ''
+surname = ''
+age = 0
+@bot.message_handler(content_types=['text'])
+def start(message):
+    if message.text == '/start':
+        bot.send_message(message.from_user.id, "Yeah")
+    else:
+        bot.send_message(message.from_user.id, 'Напиши /start')
+
+
+bot.polling(none_stop=True, interval=0)
