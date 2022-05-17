9257dc3e9b332435a8964657f3d129d3cc615dc0
annabsmirnova <90455502+annabsmirnova@users.noreply.github.com>
2022-05-17 22:54:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import telebot
-bot = telebot.TeleBot('5302345860:AAGahsIU7Q6lAYz4tD5ZVVFMpqugRKTHXIE')
+import requests
+from telebot import types
+from geopy.geocoders import Nominatim
 
-name = ''
-surname = ''
-age = 0
-@bot.message_handler(content_types=['text'])
+bot = telebot.TeleBot('5302345860:AAGahsIU7Q6lAYz4tD5ZVVFMpqugRKTHXIE')
+geolocator = Nominatim(user_agent='why')
+
+
+@bot.message_handler(commands=["start"])
 def start(message):
-    if message.text == '/start':
-        bot.send_message(message.from_user.id, "Yeah")
-    else:
-        bot.send_message(message.from_user.id, 'Напиши /start')
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_location = types.KeyboardButton(text="Поделиться местоположением", request_location=True)
+    keyboard.add(button_location)
+    bot.send_message(message.chat.id, "Поделитесь вашим местоположением (должна быть включена геолокация) ",
+                     reply_markup=keyboard)
+
+
+@bot.message_handler(content_types=['location'])
+def location(message):
+    if message.location is not None:
+        print(message.location)
+        print(message)
+        long = message.location.longitude
+        lat = message.location.latitude
+        str_coordinates = str(lat) + ", " + str(long)
+        location = geolocator.reverse(str_coordinates)
+        print(location.address)
 
 
 bot.polling(none_stop=True, interval=0)
-print('Hi')
